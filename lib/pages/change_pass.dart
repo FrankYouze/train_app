d092dc670f11d5a5b24fb7a6bfcc16d7df09332c
FrankYouze <frankyouze22@gmail.com>
2024-05-30 01:46:38 +0300

    30th may

--- lib/pages/change_pass.dart
+++ lib/pages/change_pass.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:train_app/components/my_button.dart';
 import 'package:train_app/components/my_textfield.dart';
+import 'package:train_app/pages/login_page.dart';
 
 class ChangePass extends StatefulWidget {
   const ChangePass({ Key? key }) : super(key: key);
@@ -18,37 +19,43 @@ class _ChangePassState extends State<ChangePass> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: SingleChildScrollView(child: Center(
-        child: Column(children: [
-          Text("Change user password"),
-          SizedBox(height: 20,),
-          MyTextField(controller: old, myText: "enter old password", obscu: true),
-          SizedBox(height: 20,),
-          MyTextField(controller: new1, myText: "enter new password", obscu: true),
-          SizedBox(height: 20,),
-          MyTextField(controller: new2, myText: "confirm new password", obscu: true),
-          SizedBox(height: 20,),
-          MyButton(onTap: () async{
- User? user = FirebaseAuth.instance.currentUser;
-
-  if (user != null && new1.text == new2.text) {
-    try {
-
-
-      await user.updatePassword(new2.text);
-      print("Password changed successfully");
-    } catch (e) {
-      print("Error changing password: $e");
-    }
-  } else {
-    print("No user is currently signed in.");
-  }
-
-
-          })
-
-        ],),
-      ),),
+      body:SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(children: [
+              Text("Change user password"),
+              SizedBox(height: 20,),
+              MyTextField(controller: old, myText: "enter old password", obscu: true),
+              SizedBox(height: 20,),
+              MyTextField(controller: new1, myText: "enter new password", obscu: true),
+              SizedBox(height: 20,),
+              MyTextField(controller: new2, myText: "confirm new password", obscu: true),
+              SizedBox(height: 20,),
+              MyButton(onTap: () async{
+           User? user = FirebaseAuth.instance.currentUser;
+          
+            if (user != null && new1.text == new2.text) {
+              try {
+          
+          
+          await user.updatePassword(new2.text).then((value) => {
+            Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage()))
+          });
+          print("Password changed successfully");
+              } catch (e) {
+          print("Error changing password: $e");
+              }
+            } else {
+              print("No user is currently signed in.");
+            }
+          
+          
+              })
+          
+            ],),
+          ),
+        ),
+      ),
       
     );
   }
